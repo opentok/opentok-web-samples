37f3642edf120c3dbc5ff9ab9dddad528d48755c
Hashir Baqai <hashirbaqai@gmail.com>
2017-06-07 14:11:40 -0700

    updating comment

--- Archiving/js/config.js
+++ Archiving/js/config.js
@@ -1,8 +1,10 @@
 // Make a copy of this file and save it as config.js (in the js directory).
-
-// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
-// Do include http:// for localhost.
+//
+// Set SAMPLE_SERVER_BASE_URL to your server's domain name
+// for example: 'https://yourdomain.com'.
+//
+// If you are using localhost, make sure to include http:// 
 // Do not include the trailing slash. 
-// See the README for more information:
+// See the README for more information
 
-var SAMPLE_SERVER_BASE_URL = 'http://localhost:8080';
+var SAMPLE_SERVER_BASE_URL = 'http://yourdomain.com';
