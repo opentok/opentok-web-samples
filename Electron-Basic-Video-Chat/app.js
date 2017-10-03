934e9177014a6afcd7298219e5525bcc8887839b
Manik <msach22@gmail.com>
2017-10-03 11:58:44 -0700

    getting ready for public

--- Electron-Basic-Video-Chat/app.js
+++ Electron-Basic-Video-Chat/app.js
@@ -22,15 +22,12 @@ const createWindow = () => {
     slashes: true
   }));
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = null;
   });
 };
 
--- Electron-Basic-Video-Chat/openTok.js
+++ Electron-Basic-Video-Chat/openTok.js
@@ -1,20 +1,22 @@
-//Set Credentials
+// Set Credentials
 const apiKey = "";
 const sessionId = "";
 const token = "";
 
-//Initialize Session
+// Initialize Session
 const session = OT.initSession(apiKey, sessionId);
 
-//Set session event listeners
+// Set session event listeners
 session.on({
   streamCreated: (event) => {
     session.subscribe(event.stream, 'subscriber', (error) => {
-      if (error) console.log(`There was an issue subscribing to the stream ${event}`);
+      if (error) {
+        console.log(`There was an issue subscribing to the stream ${event}`);
+      }
     });
   },
   streamDestroyed: (event) => {
-    console.log(`Stream ${event.stream.name} ended ${event.reason}`);
+    console.log(`Stream with name ${event.stream.name} ended because of reason: ${event.reason}`);
   }
 })
 
@@ -22,17 +24,17 @@ session.on({
 session.connect(token, (error) => {
   // If the connection is successful, initialize a publisher and publish to the session
   if (error) {
-    return console.log('error connecting to session', error);
+    return console.log(`There was an error connecting to session: ${error}`);
   } else {
     // Create a publisher
     const publisher = OT.initPublisher('publisher', (error) => {
       if (error) {
-        return console.log('There was an error intitializing the publisher', error);
+        return console.log(`There was an error initializing the publisher: ${error}`);
       }
     });
     session.publish(publisher, (error) => {
       if (error) {
-        return console.log('There was an error publishing', error);
+        return console.log(`There was an error when trying to publish: ${error}`);
       }
     });
   }
