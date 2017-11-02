d9e19f2badef142fb8e0be0db629a2e3c3ce7af8
Adam Ullman <adam@tokbox.com>
2017-11-02 22:50:18 +1100

    Beginnings of a Vue js sample application

---
+++ Vue-Basic-Video-Chat/js/app.js
@@ -0,0 +1,105 @@
+Vue.component('publisher', {
+  template: '<div></div>',
+  props: {
+    session: {
+      type: OT.Session,
+      required: false
+    },
+    opts: {
+      type: Object,
+      required: false
+    }
+  },
+  mounted: function() {
+    publisher = OT.initPublisher(this.$el, this.opts, (err) => {
+      if (err) {
+        this.$emit('error', err);
+      } else {
+        this.$emit('publisherCompleted');
+      }
+    });
+    this.$emit('publisherCreated', publisher);
+    if (this.session && this.session.isConnected()) {
+      this.session.publish(publisher);
+    } else {
+      this.session.on('sessionConnected', () => {
+        this.session.publish(publisher);
+      });
+    }
+  }
+});
+
+Vue.component('subscriber', {
+  props: {
+    stream: {
+      type: OT.Stream,
+      required: true
+    },
+    session: {
+      type: OT.Session,
+      required: true
+    },
+    opts: {
+      type: Object,
+      required: false
+    }
+  },
+  template: '<div></div>',
+  mounted: function() {
+    const subscriber = this.session.subscribe(this.stream, this.$el, this.opts, (err) => {
+      if (err) {
+        this.$emit('error', err);
+      } else {
+        this.$emit('subscriberConnected', subscriber);
+      }
+    });
+    this.$emit('subscriberCreated', subscriber);
+  }
+});
+
+function initializeSession(apiKey, sessionId, token) {
+  new Vue({
+    el: '#session',
+    data: {
+      streams: [],
+      connections: [],
+      connected: false,
+      session: null
+    },
+    created: function() {
+      this.session = OT.initSession(apiKey, sessionId);
+      this.session.on('streamCreated', (event) => {
+        this.streams.push(event.stream);
+        this.$emit('streamCreated', event);
+      });
+      this.session.on('streamDestroyed', (event) => {
+        const idx = this.streams.indexOf(event.stream);
+        if (idx > -1) {
+          this.streams.splice(idx, 1);
+        }
+      });
+      this.session.connect(token, (err) => {
+        if (err) {
+          this.errorHandler(err);
+        } else {
+          this.connected = true;
+        }
+      });
+    },
+    methods: {
+      errorHandler: function(err) {
+        alert(err.message);
+      }
+    }
+  });
+}
+
+if (API_KEY && TOKEN && SESSION_ID) {
+  initializeSession(API_KEY, SESSION_ID, TOKEN);
+} else {
+  fetch(SAMPLE_SERVER_BASE_URL + '/session')
+    .then((data) => data.json())
+    .then((json) => {
+      initializeSession(json.apiKey, json.sessionId, json.token);
+  });
+}
---
+++ Vue-Basic-Video-Chat/js/config.js
@@ -0,0 +1,14 @@
+// Make a copy of this file and save it as config.js (in the js directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+var SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
+
+// OR, if you have not set up a web server that runs the learning-opentok-php code,
+// set these values to OpenTok API key, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tokbox.com/account.
+
+var API_KEY = '1127';
+var SESSION_ID = '1_MX4xMTI3fn4xNTA5NjIzMDc0Njc5fitCMlQxeDJ6c2tZbGM5TGxLTERzNkEwbX5-';
+var TOKEN = 'T1==cGFydG5lcl9pZD0xMTI3JnNpZz1jYmEwMWRmMTI2YTJkMWU0NzQ1YTZjOTM5MDAzMGQ5NWUwZWFhZWM4OnNlc3Npb25faWQ9MV9NWDR4TVRJM2ZuNHhOVEE1TmpJek1EYzBOamM1Zml0Q01sUXhlREo2YzJ0WmJHTTVUR3hMVEVSek5rRXdiWDUtJmNyZWF0ZV90aW1lPTE1MDk2MjMwNzUmbm9uY2U9MC4yMDU5MTc1MDg0NDU2MjM3MyZyb2xlPW1vZGVyYXRvciZleHBpcmVfdGltZT0xNTEyMjE1MDc1JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9';
