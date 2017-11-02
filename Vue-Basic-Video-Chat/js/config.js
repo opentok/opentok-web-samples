e537911bb1b5a5d7fadd3323e79fd94e368cdb0c
Adam Ullman <adam@tokbox.com>
2017-11-02 22:52:52 +1100

    Remove apiKey, sessionId and token from config

--- Vue-Basic-Video-Chat/js/config.js
+++ Vue-Basic-Video-Chat/js/config.js
@@ -9,6 +9,6 @@ var SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
 // set these values to OpenTok API key, a valid session ID, and a token for the session.
 // For test purposes, you can obtain these from https://tokbox.com/account.
 
-var API_KEY = '1127';
-var SESSION_ID = '1_MX4xMTI3fn4xNTA5NjIzMDc0Njc5fitCMlQxeDJ6c2tZbGM5TGxLTERzNkEwbX5-';
-var TOKEN = 'T1==cGFydG5lcl9pZD0xMTI3JnNpZz1jYmEwMWRmMTI2YTJkMWU0NzQ1YTZjOTM5MDAzMGQ5NWUwZWFhZWM4OnNlc3Npb25faWQ9MV9NWDR4TVRJM2ZuNHhOVEE1TmpJek1EYzBOamM1Zml0Q01sUXhlREo2YzJ0WmJHTTVUR3hMVEVSek5rRXdiWDUtJmNyZWF0ZV90aW1lPTE1MDk2MjMwNzUmbm9uY2U9MC4yMDU5MTc1MDg0NDU2MjM3MyZyb2xlPW1vZGVyYXRvciZleHBpcmVfdGltZT0xNTEyMjE1MDc1JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9';
+var API_KEY = '';
+var SESSION_ID = '';
+var TOKEN = '';
