6f9622d4f907a871a25acb255d5f646ba1204638
Adam Ullman <adam@tokbox.com>
2018-02-01 14:22:46 +1100

    Run on localhost
    Because that's what has permission to access media in the UserMediaPermissions.plist file

--- Basic Video Chat/test/test.js
+++ Basic Video Chat/test/test.js
@@ -3,7 +3,7 @@
 var assert = require('assert');
 
 describe('webdriver.io page', () => {
-  const URL = 'http://localhost:8080/Basic Video Chat';
+  const URL = 'http://localhost/Basic Video Chat';
   beforeAll(() => {
     browser.url(URL);
   });
--- wdio.conf.js
+++ wdio.conf.js
@@ -71,7 +71,7 @@ let config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: 'http://localhost:8080',
+  baseUrl: 'http://localhost',
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
