cb6b222e5e581eadf9dcd3e0b737ef2dbd4a3840
Adam Ullman <adam@tokbox.com>
2018-02-02 15:33:54 +1100

    Use const instead of var

--- Basic Video Chat/test/test-chrome-firefox-safari-ie.js
+++ Basic Video Chat/test/test-chrome-firefox-safari-ie.js
@@ -1,6 +1,6 @@
 /* global browser describe it beforeAll $ */
 
-var assert = require('assert');
+const assert = require('assert');
 
 describe('Basic Video Chat Test', () => {
   beforeAll(() => {
@@ -8,18 +8,18 @@ describe('Basic Video Chat Test', () => {
   });
 
   it('should have the right title', () => {
-    var title = browser.getTitle();
+    const title = browser.getTitle();
     assert.equal(title, 'OpenTok Getting Started');
   });
 
   it('The publisher should load', () => {
-    var publisher = $('div.OT_publisher:not(.OT_loading) .OT_video-element');
+    const publisher = $('div.OT_publisher:not(.OT_loading) .OT_video-element');
     publisher.waitForExist(10000);
   });
 
   it('The subscriber should load if you open a new window', () => {
     browser.execute('window.open(window.location.href)');
-    var subscriber = $('div.OT_subscriber:not(.OT_loading) .OT_video-element');
+    const subscriber = $('div.OT_subscriber:not(.OT_loading) .OT_video-element');
     subscriber.waitForExist(10000);
   });
 });
