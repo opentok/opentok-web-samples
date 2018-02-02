6b8aa0b97e2dcca9f2ca120456e5ab171b5efa9d
Adam Ullman <adam@tokbox.com>
2018-02-02 14:32:46 +1100

    Wait longer for the Publisher to show up
    IE can take some time to download the fake devices video

--- Basic Video Chat/test/test.js
+++ Basic Video Chat/test/test.js
@@ -14,7 +14,7 @@ describe('webdriver.io page', () => {
 
   it('The publisher should load', () => {
     var publisher = $('div.OT_publisher:not(.OT_loading) .OT_video-element');
-    publisher.waitForExist(5000);
+    publisher.waitForExist(10000);
   });
 
   it('The subscriber should load if you open a new window', () => {
