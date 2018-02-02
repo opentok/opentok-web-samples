344b50dc275f9c903a4edbc74e135bc630e0a3e5
Adam Ullman <adam@tokbox.com>
2018-02-02 15:40:29 +1100

    Only allow 1 instance to run.
    Don't fork multiple driver instances

--- wdio.conf.js
+++ wdio.conf.js
@@ -38,7 +38,7 @@ const config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  maxInstances: 10,
+  maxInstances: 1,
   //
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
