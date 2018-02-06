ea51944e926467c98a8ca9d67f7b02197f100864
Adam Ullman <adam@tokbox.com>
2018-02-06 15:23:16 +1100

    Remove the classlist and web animation polyfills
    It seems like they were breaking the binding in IE 11 for some reason.

--- Angular-Basic-Video-Chat/src/polyfills.ts
+++ Angular-Basic-Video-Chat/src/polyfills.ts
@@ -40,7 +40,7 @@ import 'core-js/es6/set';
 
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js';  // Run `npm install --save classlist.js`.
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following for the Reflect API. */
 import 'core-js/es6/reflect';
@@ -55,7 +55,7 @@ import 'core-js/es7/reflect';
  * Required to support Web Animations `@angular/platform-browser/animations`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
-import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 
