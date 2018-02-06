b56eb789cdf341d04e43b6ded9b1618a40fc1e3d
Adam Ullman <adam@tokbox.com>
2018-02-06 16:08:52 +1100

    IE needs a little more time

--- Signaling/test/test-chrome-firefox-safari-ie.js
+++ Signaling/test/test-chrome-firefox-safari-ie.js
@@ -31,7 +31,7 @@ describe('Signaling Test', () => {
       subscriber.waitForExist(10000);
       browser.switchTab(firstTabId);
       const subscriber2 = $('div.OT_subscriber:not(.OT_loading) .OT_video-element');
-      subscriber2.waitForExist(5000);
+      subscriber2.waitForExist(10000);
     });
 
     it('You should be able to send text from one tab to another', () => {
@@ -43,14 +43,14 @@ describe('Signaling Test', () => {
 
       // Make sure my own text showed up in the text chat
       const myHistoryItem = $('#history p.mine');
-      myHistoryItem.waitForExist(1000);
+      myHistoryItem.waitForExist(2000);
       const myHistoryText = browser.getText('#history p.mine');
       assert.equal(myHistoryText, 'hello world');
 
       // switch to other tab and make sure they got the message
       browser.switchTab(secondTabId);
       const theirHistoryItem = $('#history p.theirs');
-      theirHistoryItem.waitForExist(1000);
+      theirHistoryItem.waitForExist(2000);
       const theirHistoryText = browser.getText('#history p.theirs');
       assert.equal(theirHistoryText, 'hello world');
     });
