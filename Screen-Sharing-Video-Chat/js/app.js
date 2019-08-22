d768aaf382057b0849c2abf1abcb31921b4d9831
Tommy Karkay Afik <tommy.karkayafik@vonage.com>
2019-08-22 11:52:43 +0300

    removed logging + travis fixes

--- Screen-Sharing-Video-Chat/js/app.js
+++ Screen-Sharing-Video-Chat/js/app.js
@@ -8,7 +8,7 @@ var screenSubscriber;
 var session;
 var cameraPublisher;
 var cameraSubscriber;
-var elements = {'screen-subscriber' : document.getElementById('screen-subscriber'), 'camera-subscriber' : document.getElementById('camera-subscriber'), 'screen-publisher' : document.getElementById('screen-publisher')}
+var elements = {'screen-subscriber': document.getElementById('screen-subscriber'), 'camera-subscriber': document.getElementById('camera-subscriber'), 'screen-publisher': document.getElementById('screen-publisher')};
 
 // Handling all of our errors here by alerting them
 function handleError(error) {
@@ -36,25 +36,20 @@ function switchWithBig(pressed) {
 
 function rearrangeElements() {
   Object.keys(elements).forEach(element => elements[element].classList.forEach(cls => elements[element].classList.remove(cls)));
-  //alert('aaaaa');
-  console.log("classlist for screen-subscriber")
   if (screenSubscriber && screenSubscriber.stream) {
-    elements['screen-subscriber'].classList.add('big-subscriber')
-    //alert('bbbbb');
+    elements['screen-subscriber'].classList.add('big-subscriber');
   } else if (screenPublisher && screenPublisher.session) {
-    elements['screen-publisher'].classList.add('big-subscriber')
-    //alert('ccccc');
+    elements['screen-publisher'].classList.add('big-subscriber');
   } else if (cameraSubscriber && cameraSubscriber.stream) {
-    elements['camera-subscriber'].classList.add('big-subscriber')
-    //alert('ddddd');
+    elements['camera-subscriber'].classList.add('big-subscriber');
   }
 
   if (screenPublisher && screenPublisher.session && !(elements['screen-publisher'].classList.contains('big-subscriber'))) {
-    elements['screen-publisher'].classList.add('small-screen')
+    elements['screen-publisher'].classList.add('small-screen');
   }
 
   if (cameraSubscriber && cameraSubscriber.session && !(elements['camera-subscriber'].classList.contains('big-subscriber'))) {
-    elements['camera-subscriber'].classList.add('small-camera')
+    elements['camera-subscriber'].classList.add('small-camera');
   }
 }
 
@@ -69,22 +64,22 @@ function toggleScreen() {
   // Else, check whether screen sharing is possible. If possible, initialize screen sharing publisher
   } else if (cameraPublisher.session) {
     OT.checkScreenSharingCapability(function checkScreenSharingCapability(response) {
-    if (!response.supported || response.extensionRegistered === false) {
-      alert('screen sharing not supported');
-    } else if (response.extensionInstalled === false) {
-      alert('screen sharing extension required, please install one to share your screen');
-    } else {
-      screenPublisher = OT.initPublisher('screen-publisher', {
-        insertMode: 'append',
-        width: '100%',
-        height: '100%',
-        videoSource: 'screen'
-      }, handleError);
-      if (session && session.connection) {
-        session.publish(screenPublisher,handleError)
+      if (!response.supported || response.extensionRegistered === false) {
+        alert('screen sharing not supported');
+      } else if (response.extensionInstalled === false) {
+        alert('screen sharing extension required, please install one to share your screen');
+      } else {
+        screenPublisher = OT.initPublisher('screen-publisher', {
+          insertMode: 'append',
+          width: '100%',
+          height: '100%',
+          videoSource: 'screen'
+        }, handleError);
+        if (session && session.connection) {
+          session.publish(screenPublisher, handleError);
+        }
       }
-    }
-  });
+    });
     screenSharing.classList.add('toggle-button-on');
     screenSharing.classList.remove('toggle-button-off');
   }
@@ -133,9 +128,8 @@ function initializeSession() {
 
   // When the publish screen button is pressed, call toggleScreen
   document.getElementById('screen-sharing').addEventListener('click', toggleScreen);
-  
-  ['screen-subscriber','screen-publisher'].forEach(element => document.getElementById(element).addEventListener('click', () => switchWithBig(document.getElementById(element))));
 
+  ['screen-subscriber', 'screen-publisher'].forEach(element => document.getElementById(element).addEventListener('click', () => switchWithBig(document.getElementById(element))));
 }
 
 // See the config.js file.
