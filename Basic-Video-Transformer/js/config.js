6f5bbf19e6251e1b34062198a2590e66b114fdfc
Christian Pettet <71297280+cpettet@users.noreply.github.com>
2022-10-27 15:45:25 -0400

    no config

--- Basic-Video-Transformer/js/config.js
+++ Basic-Video-Transformer/js/config.js
@@ -10,6 +10,6 @@ var SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
 // set these values to OpenTok API key, a valid session ID, and a token for the session.
 // For test purposes, you can obtain these from https://tokbox.com/account.
 
-var API_KEY = '100';
-var SESSION_ID = '1_MX4xMDB-fjE2NjY4ODM1NTQ2MDV-cHk0aUxwZEVjN0l3V2pjMDhYb280NWlrfn5-';
-var TOKEN = 'T1==cGFydG5lcl9pZD0xMDAmc2lnPTYxZTk1YzYzYTE4MDEyNjM2ZTU4NGIyZTljMDEyMTgyM2E5NDk1Njg6c2Vzc2lvbl9pZD0xX01YNHhNREItZmpFMk5qWTRPRE0xTlRRMk1EVi1jSGswYVV4d1pFVmpOMGwzVjJwak1EaFliMjgwTldscmZuNS0mY3JlYXRlX3RpbWU9MTY2Njg4MzU1NSZub25jZT0wLjY3ODM3OTM0NTc0ODM5Mjkmcm9sZT1tb2RlcmF0b3ImZXhwaXJlX3RpbWU9MTY2Njk2OTk1NSZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==';
+var API_KEY = '';
+var SESSION_ID = '';
+var TOKEN = '';
