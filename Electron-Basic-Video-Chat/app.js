ae05e05588d54a0155a8118291d11d93ca892e85
Michael Whittemore <michael.whittemore@vonage.com>
2022-10-31 13:50:48 -0700

    updated version and added node integration

--- Electron-Basic-Video-Chat/app.js
+++ Electron-Basic-Video-Chat/app.js
@@ -13,7 +13,14 @@ let mainWindow;
 
 const createWindow = () => {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+  }
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
