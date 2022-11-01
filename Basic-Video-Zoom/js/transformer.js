756f3ce5494b121e37d473d42ab58ddd9bd64e73
iujie <iujie.lim@vonage.com>
2022-11-01 18:36:45 +0800

    add face tracking

--- Basic-Video-Zoom/js/transformer.js
+++ Basic-Video-Zoom/js/transformer.js
@@ -6,6 +6,8 @@ const FACE_DETECTION_TIME_GAP = 20000;
 export default class LouReedTransformer {
   mediapipeResult_;
   visibleRectDimension;
+  frameMovingSteps;
+  visibleRectDimensionState;
 
   constructor() {
     console.log('transformer initializing');
@@ -14,7 +16,13 @@ export default class LouReedTransformer {
         width: 30,
         height: 120
     }
-    this.videoInfo = null
+    this.videoInfo = null;
+    this.frameMovingSteps = {
+      x: 1,
+      y: 1,
+      width: 1,
+      height: 1
+  }
   }
 
   start() {
@@ -41,7 +49,8 @@ export default class LouReedTransformer {
     if (!this.videoInfo) {
       this.videoInfo = {
         width: frame.displayWidth,
-        height: frame.displayHeight
+        height: frame.displayHeight,
+        frameRate: 30 // Set to 30, ideal case: get actual framerate from camera track
       }
     }
 
@@ -62,12 +71,31 @@ export default class LouReedTransformer {
     }
 
     if (this.visibleRectDimension) {
+      if (this.visibleRectDimensionState.visibleRectX !== this.visibleRectDimension.visibleRectX) this.visibleRectDimensionState.visibleRectX = this.visibleRectDimensionState.visibleRectX > this.visibleRectDimension.visibleRectX ? this.visibleRectDimensionState.visibleRectX - this.frameMovingSteps.x : this.visibleRectDimensionState.visibleRectX + this.frameMovingSteps.x ;
+      if (this.visibleRectDimensionState.visibleRectY !== this.visibleRectDimension.visibleRectY) this.visibleRectDimensionState.visibleRectY = this.visibleRectDimensionState.visibleRectY > this.visibleRectDimension.visibleRectY ? this.visibleRectDimensionState.visibleRectY - this.frameMovingSteps.y : this.visibleRectDimensionState.visibleRectY + this.frameMovingSteps.y;
+      if (this.visibleRectDimensionState.visibleRectWidth !== this.visibleRectDimension.visibleRectWidth) this.visibleRectDimensionState.visibleRectWidth = this.visibleRectDimensionState.visibleRectWidth > this.visibleRectDimension.visibleRectWidth ? this.visibleRectDimensionState.visibleRectWidth - this.frameMovingSteps.width : this.visibleRectDimensionState.visibleRectWidth + this.frameMovingSteps.width;
+      if (this.visibleRectDimensionState.visibleRectHeight !== this.visibleRectDimension.visibleRectHeight) this.visibleRectDimensionState.visibleRectHeight = this.visibleRectDimensionState.visibleRectHeight > this.visibleRectDimension.visibleRectHeight ? this.visibleRectDimensionState.visibleRectHeight - this.frameMovingSteps.height : this.visibleRectDimensionState.visibleRectHeight + this.frameMovingSteps.height;
+      
+      if (Math.abs(this.visibleRectDimensionState.visibleRectX - this.visibleRectDimension.visibleRectX) <= this.frameMovingSteps.x) this.visibleRectDimensionState.visibleRectX = this.visibleRectDimension.visibleRectX
+      if (Math.abs(this.visibleRectDimensionState.visibleRectY  - this.visibleRectDimension.visibleRectY) <= this.frameMovingSteps.y) this.visibleRectDimensionState.visibleRectY = this.visibleRectDimension.visibleRectY
+      if (Math.abs(this.visibleRectDimensionState.visibleRectWidth  - this.visibleRectDimension.visibleRectWidth) <= this.frameMovingSteps.width) this.visibleRectDimensionState.visibleRectWidth = this.visibleRectDimension.visibleRectWidth
+      if (Math.abs(this.visibleRectDimensionState.visibleRectHeight  - this.visibleRectDimension.visibleRectHeight) <= this.frameMovingSteps.height) this.visibleRectDimensionState.visibleRectHeight = this.visibleRectDimension.visibleRectHeight
+      
+      let deltaX = this.visibleRectDimensionState.visibleRectX + this.visibleRectDimensionState.visibleRectWidth;
+      let deltaY = this.visibleRectDimensionState.visibleRectY + this.visibleRectDimensionState.visibleRectHeight;
+
+      if (deltaX > this.videoInfo.width) this.visibleRectDimensionState.visibleRectWidth = this.visibleRectDimensionState.visibleRectWidth - (deltaX - this.videoInfo.width);
+      if (deltaY > this.videoInfo.height) this.visibleRectDimensionState.visibleRectHeight = this.visibleRectDimensionState.visibleRectHeight - (deltaY - this.videoInfo.height);
+
+      console.log("State1", this.visibleRectDimensionState.visibleRectX, " ", this.visibleRectDimensionState.visibleRectY, " ", this.visibleRectDimensionState.visibleRectWidth, " ", this.visibleRectDimensionState.visibleRectHeight)
+      console.log("State2", this.visibleRectDimension.visibleRectX, " ", this.visibleRectDimension.visibleRectY, " ", this.visibleRectDimension.visibleRectWidth, " ", this.visibleRectDimension.visibleRectHeight)
+
       const resizeFrame = new VideoFrame(image, {
         visibleRect: {
-            x: this.visibleRectDimension.visibleRectX,
-            y: this.visibleRectDimension.visibleRectY,
-            width: this.visibleRectDimension.visibleRectWidth,
-            height: this.visibleRectDimension.visibleRectHeight
+            x: this.visibleRectDimensionState.visibleRectX,
+            y: this.visibleRectDimensionState.visibleRectY,
+            width: this.visibleRectDimensionState.visibleRectWidth,
+            height: this.visibleRectDimensionState.visibleRectHeight
         },
         timestamp,
         alpha: 'discard'
@@ -91,7 +119,7 @@ calculateDimensions() {
     newY = Math.max(0, newY);
 
     
-    if (!this.visibleRectDimension || Math.abs(newX - this.visibleRectDimension.visibleRectX) >=  1 || Math.abs(newY - this.visibleRectDimension.visibleRectY) >= 1 ) {
+    if (!this.visibleRectDimension || Math.abs(newX - this.visibleRectDimension.visibleRectX) > 10 || Math.abs(newY - this.visibleRectDimension.visibleRectY) > 10 ) {
         // Ensure x and y is even value
         let visibleRectX = (( newX % 2) === 0) ? newX : (newX + 1);
         let visibleRectY = (( newY % 2) === 0) ? newY : (newY + 1);
@@ -104,6 +132,15 @@ calculateDimensions() {
         visibleRectY,
         visibleRectWidth,
         visibleRectHeight
+        }
+        if (!this.visibleRectDimensionState) this.visibleRectDimensionState = this.visibleRectDimension;
+        else {
+            this.frameMovingSteps= {
+                x: Math.max(Math.floor(Math.abs(this.visibleRectDimensionState.visibleRectX - this.visibleRectDimension.visibleRectX)/(this.videoInfo.frameRate/5)), 1),
+                y: Math.max(Math.floor(Math.abs(this.visibleRectDimensionState.visibleRectY - this.visibleRectDimension.visibleRectY)/(this.videoInfo.frameRate/5)),1),
+                width: Math.max(Math.floor(Math.abs(this.visibleRectDimensionState.visibleRectWidth - this.visibleRectDimension.visibleRectWidth)/(this.videoInfo.frameRate/5)),1),
+                height: Math.max(Math.floor(Math.abs(this.visibleRectDimensionState.visibleRectHeight - this.visibleRectDimension.visibleRectHeight)/(this.videoInfo.frameRate/5)),1)
+            }
         }
     }
 }
