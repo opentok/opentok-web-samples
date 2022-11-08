2a2ce7bba929fe0362123ca73680350afce07f30
Christian Pettet <71297280+cpettet@users.noreply.github.com>
2022-11-08 17:13:39 -0500

    keys

--- Basic-Background-Blur/js/config.js
+++ Basic-Background-Blur/js/config.js
@@ -10,6 +10,6 @@ var SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
 // set these values to OpenTok API key, a valid session ID, and a token for the session.
 // For test purposes, you can obtain these from https://tokbox.com/account.
 
-var API_KEY = '100';
-var SESSION_ID = '1_MX4xMDB-fjE2Njc5NDUwMTQwOTZ-MDIzL1hlQTh4akdQbTZuK2dCYzJrNmgzfn5-';
-var TOKEN = 'T1==cGFydG5lcl9pZD0xMDAmc2lnPTU1YzQwMDY1MGE1ZjIyNzA5ZWQxNmY2ZGVlZjkyODY3YWJkNzcxYmQ6c2Vzc2lvbl9pZD0xX01YNHhNREItZmpFMk5qYzVORFV3TVRRd09UWi1NREl6TDFobFFUaDRha2RRYlRadUsyZENZekpyTm1nemZuNS0mY3JlYXRlX3RpbWU9MTY2Nzk0NTAxNCZub25jZT0wLjc0NzA3ODI2MTkwMDU1MDkmcm9sZT1tb2RlcmF0b3ImZXhwaXJlX3RpbWU9MTY2ODAzMTQxNCZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==';
+var API_KEY = '';
+var SESSION_ID = '';
+var TOKEN = '';
