fa64246431fb585b4105b52e3eba2f69b1e5a7c7
Dwane <dwane.hemmings@vonage.com>
2023-02-08 13:56:04 -0500

    double check and clean up

--- Publish-Canvas/js/config.js
+++ Publish-Canvas/js/config.js
@@ -4,8 +4,7 @@
 // Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
 // Do not include the trailing slash. See the README for more information:
 
-// const SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
-const SAMPLE_SERVER_BASE_URL = 'https://web-production-e15d.up.railway.app';
+const SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
 
 // OR, if you have not set up a web server that runs the learning-opentok-php code,
 // set these values to OpenTok API key, a valid session ID, and a token for the session.
