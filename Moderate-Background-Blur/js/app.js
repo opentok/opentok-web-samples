fe63241ae5f39798da31ddab69eaab52deacfe55
Dwane <dwane.hemmings@vonage.com>
2023-02-17 13:59:23 -0500

    create new directory and updated files and packages

---
+++ Moderate-Background-Blur/js/app.js
@@ -0,0 +1,90 @@
+import {
+  BlurRadius,
+  createVonageMediaProcessor
+} from '../node_modules/@vonage/ml-transformers/dist/ml-transformers.es.js';
+/* global OT API_KEY TOKEN SESSION_ID SAMPLE_SERVER_BASE_URL */
+/* global MediaProcessorConnector */
+
+let apiKey;
+let sessionId;
+let token;
+
+const config = {
+  transformerType: 'BackgroundBlur',
+  radius: BlurRadius.High
+};
+
+const transformStream = async (publisher) => {
+  const processor = await createVonageMediaProcessor(config);
+
+  if (OT.hasMediaProcessorSupport()) {
+    publisher
+      .setVideoMediaProcessorConnector(processor.getConnector())
+      .catch((e) => {
+        console.error(e);
+      });
+  } else {
+    console.log('Browser does not support media processors');
+  }
+};
+
+const handleError = (error) => {
+  if (error) {
+    console.error(error);
+  }
+};
+
+const initializeSession = () => {
+  const session = OT.initSession(apiKey, sessionId);
+
+  // Subscribe to a newly created stream
+  session.on('streamCreated', (event) => {
+    const subscriberOptions = {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%'
+    };
+    session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+  });
+
+  // initialize the publisher
+  const publisherOptions = {
+    insertMode: 'append',
+    width: '100%',
+    height: '100%'
+  };
+  const publisher = OT.initPublisher('publisher', publisherOptions, handleError);
+
+  // Connect to the session
+  session.connect(token, async (error) => {
+    if (error) {
+      handleError(error);
+    } else {
+      // If the connection is successful, publish the publisher to the session
+      // and transform stream
+      session.publish(publisher, () => transformStream(publisher));
+    }
+  });
+};
+
+// See the config.js file.
+if (API_KEY && TOKEN && SESSION_ID) {
+  apiKey = API_KEY;
+  sessionId = SESSION_ID;
+  token = TOKEN;
+  initializeSession();
+} else if (SAMPLE_SERVER_BASE_URL) {
+  // Make a GET request to get the OpenTok API key, session ID, and token from the server
+  fetch(SAMPLE_SERVER_BASE_URL + '/session')
+  .then((response) => response.json())
+  .then((json) => {
+    apiKey = json.apiKey;
+    sessionId = json.sessionId;
+    token = json.token;
+    // Initialize an OpenTok Session object
+    initializeSession();
+  }).catch((error) => {
+    handleError(error);
+    alert('Failed to get opentok sessionId and token. Make sure you have updated the config.js file.');
+  });
+}
