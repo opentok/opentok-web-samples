f03b2003c9c0edda12307f5d1cbcc72ffd982f62
Dwane <dwane.hemmings@vonage.com>
2023-02-24 17:23:14 -0500

    updates and refactors code to also publish to the session and updates the readme and test file

--- Publish-Video/js/app.js
+++ Publish-Video/js/app.js
@@ -1,37 +1,101 @@
-/* global OT */
+/* global OT API_KEY TOKEN SESSION_ID SAMPLE_SERVER_BASE_URL */
 
-(function closure() {
-  const video = document.querySelector('#video');
-  if (!video.captureStream) {
-    alert('This browser does not support VideoElement.captureStream(). You must use Google Chrome.');
-    return;
+let apiKey;
+let sessionId;
+let token;
+const videoEl = document.querySelector('#video');
+
+function handleError(error) {
+  if (error) {
+    console.error(error);
   }
-  const stream = video.captureStream();
+}
+
+function initializeSession() {
+  const stream = videoEl.captureStream();
+  const session = OT.initSession(apiKey, sessionId);
+
+  // Subscribe to a newly created stream
+  session.on('streamCreated', (event) => {
+    const subscriberOptions = {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%'
+    };
+    session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+  });
+
+  session.on('sessionDisconnected', (event) => {
+    console.log('You were disconnected from the session.', event.reason);
+  });
+
   let publisher;
-  const publish = () => {
+  function publish() {
     const videoTracks = stream.getVideoTracks();
     const audioTracks = stream.getAudioTracks();
+
+    // initialize the publisher
+    const publisherOptions = {
+      videoSource: videoTracks[0],
+      audioSource: audioTracks[0],
+      fitMode: 'contain',
+      width: 320,
+      height: 240
+    };
     if (!publisher && videoTracks.length > 0 && audioTracks.length > 0) {
       stream.removeEventListener('addtrack', publish);
-      publisher = OT.initPublisher('publisher', {
-        videoSource: videoTracks[0],
-        audioSource: audioTracks[0],
-        fitMode: 'contain',
-        width: 320,
-        height: 240
-      }, (err) => {
+      publisher = OT.initPublisher('publisher', publisherOptions, (err) => {
         if (err) {
-          video.pause();
-          alert(err.message);
+          videoEl.pause();
+          handleError(err)
         } else {
-          video.play();
+          videoEl.play();
+          // Connect to the session
+          session.connect(token, (error) => {
+            if (error) {
+              handleError(error);
+            } else {
+              // If the connection is successful, publish the publisher to the session
+              session.publish(publisher, handleError);
+            }
+          });
         }
       });
       publisher.on('destroyed', () => {
-        video.pause();
+        videoEl.pause();
       });
     }
-  };
+  }
   stream.addEventListener('addtrack', publish);
   publish();
-})();
+}
+
+// See the config.js file.
+if (API_KEY && TOKEN && SESSION_ID) {
+  apiKey = API_KEY;
+  sessionId = SESSION_ID;
+  token = TOKEN;
+  if (!videoEl.captureStream) {
+    alert('This browser does not support VideoElement.captureStream(). You must use Google Chrome.');
+  } else {
+    initializeSession();
+  }
+} else if (SAMPLE_SERVER_BASE_URL) {
+  // Make a GET request to get the OpenTok API key, session ID, and token from the server
+  fetch(SAMPLE_SERVER_BASE_URL + '/session')
+  .then((response) => response.json())
+  .then((json) => {
+    apiKey = json.apiKey;
+    sessionId = json.sessionId;
+    token = json.token;
+    if (!videoEl.captureStream) {
+      alert('This browser does not support VideoElement.captureStream(). You must use Google Chrome.');
+    } else {
+      // Initialize an OpenTok Session object
+      initializeSession();
+    }
+  }).catch((error) => {
+    handleError(error);
+    alert('Failed to get opentok sessionId and token. Make sure you have updated the config.js file.');
+  });
+}
