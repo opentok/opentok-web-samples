c947c03793e72e0ee4887ddc79d8c725d19846f3
Dwane <dwane.hemmings@vonage.com>
2023-03-08 10:53:42 -0500

    initial refactor

--- Basic-Captions/js/app.js
+++ Basic-Captions/js/app.js
@@ -3,23 +3,36 @@
 let apiKey;
 let sessionId;
 let token;
-let captionId;
+let captionsId;
 let publisher;
 let subscriber;
+
+let captions;
 
 // clears after a set amount of time
 let captionsRemovalTimer;
 
-$(document).ready(function ready() {
-  // Make an Ajax request to get the OpenTok API key, session ID, and token from the server
-  $.get(SAMPLE_SERVER_BASE_URL + '/session', function get(res) {
-    apiKey = res.apiKey;
-    sessionId = res.sessionId;
-    token = res.token;
+const captionsStartBtn = document.querySelector('#start');
+const captionsStopBtn = document.querySelector('#stop');
 
-    initializeSession();
-  });
-});
+
+function handleError(error) {
+  if (error) {
+    console.error(error);
+  }
+}
+
+
+// $(document).ready(function ready() {
+//   // Make an Ajax request to get the OpenTok API key, session ID, and token from the server
+//   $.get(SAMPLE_SERVER_BASE_URL + '/session', function get(res) {
+//     apiKey = res.apiKey;
+//     sessionId = res.sessionId;
+//     token = res.token;
+//
+//     initializeSession();
+//   });
+// });
 
 function enableTranscription(session) {
   const { sessionId, token } = session;
@@ -31,7 +44,7 @@ function enableTranscription(session) {
       sessionId,
       token,
     }).done(function response() {
-      captionId = captionRes.responseText;
+      captionsId = captionRes.responseText;
     });
   } catch (e) {
     console.warn(e);
@@ -42,7 +55,7 @@ async function disableTranscription() {
   const url = `${SAMPLE_SERVER_BASE_URL}/captions/stop`;
   try {
     $.post(url, {
-      captionId,
+      captionsId,
     });
   } catch (e) {
     console.warn(e);
@@ -54,27 +67,29 @@ async function initializeSession() {
   let session = OT.initSession(apiKey, sessionId);
 
   // Subscribe to a newly created stream
-  session.on('streamCreated', async function streamCreated(event) {
+  session.on('streamCreated', async (event) => {
     const subscriberOptions = {
       insertMode: 'append',
       width: '100%',
       height: '100%',
       testNetwork: true,
     };
-    subscriber = session.subscribe(event.stream, 'subscriber', subscriberOptions, function callback(error) {
-      if (error) {
-        console.log('There was an error publishing: ', error.name, error.message);
-      }
-    });
+    subscriber = session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+
+    // subscriber = session.subscribe(event.stream, 'subscriber', subscriberOptions, function callback(error) {
+    //   if (error) {
+    //     console.log('There was an error publishing: ', error.name, error.message);
+    //   }
+    // });
 
     // add captions to the subscriber object
-    try {
-      await subscriber.subscribeToCaptions(true);
-    } catch (err) {
-      console.warn(err);
-    }
+    // try {
+    //   await subscriber.subscribeToCaptions(true);
+    // } catch (err) {
+    //   console.warn(err);
+    // }
 
-    subscriber.on('captionReceived', function(event){
+    subscriber.on('captionReceived', (event) => {
       const captionText = event.caption;
       const subscriberContainer = OT.subscribers.find().element;
       const [subscriberWidget] = subscriberContainer.getElementsByClassName('OT_widget-container');
@@ -95,7 +110,7 @@ async function initializeSession() {
     
       subscriberWidget.appendChild(captionBox);
     });
-    $('#stop').show();
+    // $('#stop').show();
   });
 
   session.on('sessionDisconnected', function sessionDisconnected(event) {
@@ -103,30 +118,123 @@ async function initializeSession() {
   });
 
   // Connect to the session
-  session.connect(token, async function connectCallback(error) {
-    // If the connection is successful, initialize a publisher and publish to the session
-    if (!error) {
+  session.connect(token, (error) => {
+    if (error) {
+      handleError(error);
+    } else {
+      // If the connection is successful, initialize a publisher and publish to the session
       const publisherOptions = {
         insertMode: 'append',
         width: '100%',
         height: '100%',
-        publishCaptions: true,
+        // publishCaptions: true,
       };
-      publisher = OT.initPublisher('publisher', publisherOptions, function initCallback(err) {
+      publisher = OT.initPublisher('publisher', publisherOptions, (err) => {
         if (err) {
-          console.log('There was an error initializing the publisher: ', err.name, err.message);
-          return;
+          handleError(err);
+        } else {
+          session.publish(publisher, handleError);
         }
-        session.publish(publisher, function publishCallback(pubErr) {
-          if (pubErr) {
-            console.log('There was an error publishing: ', pubErr.name, pubErr.message);
-          }
-        });
       });
-
-      enableTranscription(session);
-    } else {
-      console.log('There was an error connecting to the session: ', error.name, error.message);
     }
+
+    // If the connection is successful, initialize a publisher and publish to the session
+    // if (!error) {
+    //   const publisherOptions = {
+    //     insertMode: 'append',
+    //     width: '100%',
+    //     height: '100%',
+    //     publishCaptions: true,
+    //   };
+    //   publisher = OT.initPublisher('publisher', publisherOptions, function initCallback(err) {
+    //     if (err) {
+    //       console.log('There was an error initializing the publisher: ', err.name, err.message);
+    //       return;
+    //     }
+    //     session.publish(publisher, function publishCallback(pubErr) {
+    //       if (pubErr) {
+    //         console.log('There was an error publishing: ', pubErr.name, pubErr.message);
+    //       }
+    //     });
+    //   });
+    //
+    //   // enableTranscription(session);
+    // } else {
+    //   console.log('There was an error connecting to the session: ', error.name, error.message);
+    // }
+  });
+}
+
+async function postData(url='', data={}){
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    if (!response.ok){
+      throw new Error('error getting data!');
+    }
+    return response.json();
+  }
+  catch (error){
+    handleError(error);
+  }
+}
+
+async function startCaptions(){
+  console.log('start captions');
+  try {
+    captions = await postData(SAMPLE_SERVER_BASE_URL +'/captions/start',{sessionId, token});
+    console.log('captions started: ', captions);
+    // if (archive.status !== 'started'){
+    //   handleError(archive.error);
+    // } else {
+    //   console.log('successfully started archiving: ',archive);
+    // }
+  }
+  catch(error){
+    handleError(error);
+  }
+}
+
+async function stopCaptions(){
+  console.log('stop captions');
+  try {
+    // archive = await postData(`${SAMPLE_SERVER_BASE_URL}/archive/${archive.id}/stop`,{});
+    console.log("captions: ",captions);
+    captions = await postData(`${SAMPLE_SERVER_BASE_URL}/captions/stop`,{captionId:captionsId});
+    console.log('captions stopped: ', captions);
+    // if (archive.status !== 'stopped'){
+    //   handleError(archive.error);
+    // } else {
+    //   console.log('successfully stopped archiving: ',archive);
+    // }
+  }
+  catch(error){
+    handleError(error);
+  }
+}
+
+captionsStartBtn.addEventListener('click', startCaptions, false);
+captionsStopBtn.addEventListener('click', stopCaptions, false);
+
+
+// See the config.js file.
+if (SAMPLE_SERVER_BASE_URL) {
+  // Make a GET request to get the OpenTok API key, session ID, and token from the server
+  fetch(SAMPLE_SERVER_BASE_URL + '/session')
+      .then((response) => response.json())
+      .then((json) => {
+        apiKey = json.apiKey;
+        sessionId = json.sessionId;
+        token = json.token;
+        // Initialize an OpenTok Session object
+        initializeSession();
+      }).catch((error) => {
+    handleError(error);
+    alert('Failed to get opentok sessionId and token. Make sure you have updated the config.js file.');
   });
 }
