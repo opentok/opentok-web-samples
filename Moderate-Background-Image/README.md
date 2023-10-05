34bc7832472acf63c1a8d57510b1a8444464ee26
Ben <radhoine.ben@vonage.com>
2023-10-05 17:47:17 +0800

    add Moderate-Background-Image sample

---
+++ Moderate-Background-Image/README.md
@@ -0,0 +1,34 @@
+OpenTok.js Moderate Background Replacement
+=======================
+
+This sample application shows how to add image background replacement with the Vonage
+Video APIs. It is very similar to the [Basic Video Chat](../Basic%20Video%20Chat/) example, but it adds a background image to the publisher. See [Vonage ML Transformers](https://vonage.github.io/ml-transformers-docs/) library for more information.
+
+## Demo
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/fork/github/opentok/opentok-web-samples/tree/main/Moderate-Background-Blur)
+
+Enter your credentials in `config.js` and the application will work.
+
+> Note: There is a devDependency `sirv-cli` in the project that is only necessary to run the demo on StackBlitz.
+
+## Running the App
+
+*Important:* Read the following sections of the main README file for the repository to set up
+and test the application:
+
+* [Setting up the test web service](../README.md#setting-up-the-test-web-service)
+* [Configuring the application](../README.md#configuring-the-application)
+* [Testing the application](../README.md#testing-the-application)
+
+## Change the background image for the Video Stream
+
+After connecting to the session and publishing the audio-video stream, you can change the background image by replacing it with a new URI using the following code:
+
+```javascript
+  config.backgroundAssetUri = assetUri;
+  processor.setBackgroundOptions(config);
+```
+
+## Known Limitations
+ * MediaProcessors are only supported in recent versions of Chrome, Electron, Opera, and Edge. They are not supported in other (non-Chromium-based) browsers or on iOS. You can check if the client supports this feature by calling the `OT.hasMediaProcessorSupport()` method.
---
+++ Moderate-Background-Image/js/app.js
@@ -0,0 +1,109 @@
+import {
+  createVonageMediaProcessor
+} from '@vonage/ml-transformers/dist/ml-transformers.es.js';
+
+/* global OT API_KEY TOKEN SESSION_ID SAMPLE_SERVER_BASE_URL */
+/* global MediaProcessorConnector */
+
+const bgUrl1 = "https://neru-b617e2b2-basic-dev.euw1.runtime.vonage.cloud/bg-1.png";
+const bgUrl2 = "https://neru-b617e2b2-basic-dev.euw1.runtime.vonage.cloud/bg-2.png";
+
+let apiKey;
+let sessionId;
+let token;
+
+const config = {
+  transformerType: "VirtualBackground",
+  backgroundAssetUri: bgUrl1
+};
+
+const processor = await createVonageMediaProcessor(config);
+
+const transformStream = (publisher) => {
+
+  if (OT.hasMediaProcessorSupport()) {
+    publisher
+      .setVideoMediaProcessorConnector(processor.getConnector())
+      .catch((e) => {
+        console.error(e);
+      });
+  } else {
+    console.log('Browser does not support media processors');
+  }
+};
+
+const applyBackground = (assetUri) => {
+  config.backgroundAssetUri = assetUri;
+  processor.setBackgroundOptions(config);
+};
+
+const applyButtonsContainer = document.getElementById("buttons");
+
+applyButtonsContainer.addEventListener("click", (event) => {
+  if (event.target.id === "applyImg-1") {
+    applyBackground(bgUrl2);
+  } else if (event.target.id === "applyImg-2") {
+    applyBackground(bgUrl1);
+  }
+});
+
+const handleError = (error) => {
+  if (error) {
+    console.error(error);
+  }
+};
+
+const initializeSession = () => {
+  const session = OT.initSession(apiKey, sessionId);
+
+  // Subscribe to a newly created stream
+  session.on('streamCreated', (event) => {
+    const subscriberOptions = {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%'
+    };
+    session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+  });
+
+  // initialize the publisher
+  const publisherOptions = {
+    insertMode: 'append',
+    width: '100%',
+    height: '100%'
+  };
+  const publisher = OT.initPublisher('publisher', publisherOptions, handleError);
+
+  // Connect to the session
+  session.connect(token, (error) => {
+    if (error) {
+      handleError(error);
+    } else {
+      // If the connection is successful, publish the publisher to the session
+      // and transform stream
+      session.publish(publisher, () => transformStream(publisher));
+    }
+  });
+};
+
+// See the config.js file.
+if (API_KEY && TOKEN && SESSION_ID) {
+  apiKey = API_KEY;
+  sessionId = SESSION_ID;
+  token = TOKEN;
+  initializeSession();
+} else if (SAMPLE_SERVER_BASE_URL) {
+  // Make a GET request to get the OpenTok API key, session ID, and token from the server
+  fetch(SAMPLE_SERVER_BASE_URL + '/session')
+  .then((response) => response.json())
+  .then((json) => {
+    apiKey = json.apiKey;
+    sessionId = json.sessionId;
+    token = json.token;
+    // Initialize an OpenTok Session object
+    initializeSession();
+  }).catch((error) => {
+    handleError(error);
+    alert('Failed to get opentok sessionId and token. Make sure you have updated the config.js file.');
+  });
+}
