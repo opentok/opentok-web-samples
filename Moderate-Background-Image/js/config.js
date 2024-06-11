0a60a6b35004af60e101fa4f031228dd556f9183
Maniastrazen <manikandanm@astrazentechnologies.com>
2024-06-11 16:19:28 +0530

    Update config.js

--- Moderate-Background-Image/js/config.js
+++ Moderate-Background-Image/js/config.js
@@ -10,6 +10,8 @@ const SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
 // set these values to OpenTok API key, a valid session ID, and a token for the session.
 // For test purposes, you can obtain these from https://tokbox.com/account.
 
-const API_KEY = '';
+const API_KEY = '47913601';
+const SESSION_ID = '2_MX40NzkxMzYwMX5-MTcxODAzODA0MDg3OH5UKy9yVkxPdStlQ0dBOGYyYzl3R2JlZW9-fn4';
+const TOKEN = 'T1==cGFydG5lcl9pZD00NzkxMzYwMSZzaWc9ZmMyMDhjNTYzYzNiNDhlNDllNmNmY2Q5M2NiMjMxZWY5MDNlYjRmOTpzZXNzaW9uX2lkPTJfTVg0ME56a3hNell3TVg1LU1UY3hPREF6T0RBME1EZzNPSDVVS3k5eVZreFBkU3RsUTBkQk9HWXlZemwzUjJKbFpXOS1mbjQmY3JlYXRlX3RpbWU9MTcxODAzODA0NyZub25jZT05NTAyNCZyb2xlPVBVQkxJU0hFUg==';const API_KEY = '';
 const SESSION_ID = '';
 const TOKEN = '';
